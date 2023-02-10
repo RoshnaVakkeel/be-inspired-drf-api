081b24dcbae9d0424c1ed9bb92a51235ad33e73a
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-10 22:40:55 +0000

    Add permissions for owner only to edit posts

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,6 +8,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     '''
     # fetches user's username
     owner = serializers.ReadOnlyField(source='owner.username') 
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         '''
@@ -16,5 +21,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_on', 'updated_on', 'name',
-            'brief_bio', 'age_group', 'image',
+            'brief_bio', 'age_group', 'image', 'is_owner',
         ]
