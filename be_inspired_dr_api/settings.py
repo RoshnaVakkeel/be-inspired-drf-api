4eabeef8c3443e55c69184a29bda1d849996a901
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-27 11:44:33 +0000

    corsheaders.middleware position update

--- be_inspired_dr_api/settings.py
+++ be_inspired_dr_api/settings.py
@@ -98,6 +98,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,7 +106,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ALLOWED_HOSTS = [
